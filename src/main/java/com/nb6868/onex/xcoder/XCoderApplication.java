1383357da6579ce73e7f5076369e19f67f7a2d3e
Charles <zhangchaoxu@gmail.com>
2020-09-30 10:50:51 +0800

    add aliyun maven repo

--- src/main/java/com/nb6868/onex/xcoder/XCoderApplication.java
+++ src/main/java/com/nb6868/onex/xcoder/XCoderApplication.java
@@ -3,11 +3,16 @@ package com.nb6868.onex.xcoder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * XCoder Application
+ *
+ * @author Charles zhangchaoxu@gmail.com
+ */
 @SpringBootApplication
-public class XcoderApplication {
+public class XCoderApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(XcoderApplication.class, args);
+		SpringApplication.run(XCoderApplication.class, args);
 	}
 
 }
