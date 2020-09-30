830675ed88e5ce3499d81b5eb48a60e85f2a3bc9
Charles <zhangchaoxu@gmail.com>
2020-09-30 10:31:21 +0800

    generator by [spring initializr](https://start.spring.io/)

---
+++ src/main/java/com/nb6868/onex/xcoder/XcoderApplication.java
@@ -0,0 +1,13 @@
+package com.nb6868.onex.xcoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XcoderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XcoderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nb6868/onex/xcoder/XcoderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nb6868.onex.xcoder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XcoderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
