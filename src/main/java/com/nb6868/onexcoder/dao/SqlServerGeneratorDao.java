c337bd9c89621495b69a0dfd80d7953c1c8bf983
Charles <zhangchaoxu@gmail.com>
2020-12-15 14:25:48 +0800

    init project

---
+++ src/main/java/com/nb6868/onexcoder/dao/SqlServerGeneratorDao.java
@@ -0,0 +1,13 @@
+package com.nb6868.onexcoder.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * SQLServer代码生成器
+ *
+ * @author Charles (zhanngchaoxu@gmail.com)
+ */
+@Mapper
+public interface SqlServerGeneratorDao extends GeneratorDao {
+
+}
